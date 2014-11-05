368f26fe94968b4e73edecba5e67530344c24986
victorarcasrios <victorarcasrios@gmail.com>
2014-11-05 23:19:26 +0100

    Model added. Inserts on db

--- glade/userSigninForm.py
+++ glade/userSigninForm.py
@@ -2,10 +2,13 @@
 #-*-coding: UTF-8 -*-
 
 from gi.repository import Gtk
+import sqlite3
 
+## TODO implement methods
 class App:
 
 	def __init__(self):
+		self._model = Model()
 		self._gladeFile = "userSigninForm.glade"
 		self._glade = Gtk.Builder()
 		self._glade.add_from_file(self._gladeFile)
@@ -29,11 +32,22 @@ class App:
 	def on_saveButton_clicked(self):
 		pass
 
-##TODO implement db connection on init, list and insert methods
-# class Model:
+class Model:
 
-# 	def __init__(self):
+	def __init__(self):
+		self._conn = sqlite3.connect("practica2.db")
+		self._conn.text_factory = str
+		self._c = self._conn.cursor()
+		self._table = "users"
 
+	def insert(self, nick, password, email, name, surname, address):
+		insert = "INSERT INTO {0} VALUES (NULL, ?, ?, ?, ?, ?, ?)".format(self._table)
+		self._c.execute(insert, (nick, password, email, name, surname, address));
+		self._conn.commit();
+
+	def listAll(self):
+		self._c.execute("SELECT * FROM {0}".format(self._table))
+		return self._c.fetchall()
 
 if __name__ == "__main__":
 	app = App()
