645b2ecb88c2c71823f7b02d9f70ac18fff67a8b
victorarcasrios <victorarcasrios@gmail.com>
2014-11-30 23:46:40 +0100

    Added ConfirmDeleteDialog. Refactored Users class instance functions to @classfunctions

--- students-manager/main.py
+++ students-manager/main.py
@@ -17,41 +17,38 @@ class App():
 
 ## TODO refactor to data class w/ constructor. 
 ##		Model variable users may be a class var, not an instance one
-class GladeObject():
+class GladeContainer():
 	"""Data class for inheritance purposes only"""
 
 	glade_file = "main.glade"
-	glade = Gtk.Builder()
-	glade.add_from_file(glade_file)
 
-	def __init__(self):
-		self.users = Users()
-
-		
+	def __init__(self):		
+		self.builder = Gtk.Builder()
+		self.builder.add_from_file(self.glade_file)
 
 ###########################################################################################
 
-class MainWindow(GladeObject):
+class MainWindow(GladeContainer):
 	"""Manage main Window"""
 
 	def __init__(self):
-		GladeObject.__init__(self)
-		self.this_window 	= self.glade.get_object("MainWindow")
-		self.tree_view 		= TreeView()
+		GladeContainer.__init__(self)
+		self.this_window 	= self.builder.get_object("MainWindow")
+		self.tree_view 		= TreeView(self.builder)
 		self.entries = {
-			"surname"		: self.glade.get_object("surnameEntry"),
-			"motherSurname"	: self.glade.get_object("motherSurnameEntry"),
-			"name"			: self.glade.get_object("nameEntry"),
-			"dni"			: self.glade.get_object("dniEntry"),
-			"address"		: self.glade.get_object("addressEntry")
+			"surname"		: self.builder.get_object("surnameEntry"),
+			"motherSurname"	: self.builder.get_object("motherSurnameEntry"),
+			"name"			: self.builder.get_object("nameEntry"),
+			"dni"			: self.builder.get_object("dniEntry"),
+			"address"		: self.builder.get_object("addressEntry")
 		}
 		self.buttons = {
-			"new"			: self.glade.get_object("newUserButton"),
-			"save"			: self.glade.get_object("saveUserButton"),
-			"cancel"		: self.glade.get_object("cancelUserButton"),
-			"update"		: self.glade.get_object("updateUserButton"),
-			"delete"		: self.glade.get_object("deleteUserButton"),
-			"close"			: self.glade.get_object("closeWindowButton")
+			"new"			: self.builder.get_object("newUserButton"),
+			"save"			: self.builder.get_object("saveUserButton"),
+			"cancel"		: self.builder.get_object("cancelUserButton"),
+			"update"		: self.builder.get_object("updateUserButton"),
+			"delete"		: self.builder.get_object("deleteUserButton"),
+			"close"			: self.builder.get_object("closeWindowButton")
 		}		
 		self.this_window.connect("destroy", Gtk.main_quit)
 		self.tree_view.selection.connect("changed", self._on_tree_view_selection_changed)
@@ -60,7 +57,7 @@ class MainWindow(GladeObject):
 		self.buttons["save"].connect("clicked", self._save_data)
 		self.buttons["update"].connect("clicked", self._update_data)
 		self.buttons["cancel"].connect("clicked", self._on_cancel_clicked)
-		self.buttons["delete"].connect("clicked", self._delete_user)
+		self.buttons["delete"].connect("clicked", self._on_delete_clicked)
 		self.buttons["close"].connect("clicked", Gtk.main_quit)
 
 		## Set buttons sensitivity initial state
@@ -71,26 +68,32 @@ class MainWindow(GladeObject):
 
 	def _prepare_to_create(self, widget):
 		self._clear_entries()
-		self.entries["surname"].grab_focus()	
+		self.entries["surname"].grab_focus()
+		self.tree_view.selection.unselect_iter(self._selected_iter)	
 
 	# TODO Refactor to warning about unsuccesful insertion
 	def _save_data(self, widget):
 		data = self._get_data()
 		if self._is_proper_user_data(data):
-			self.users.insert(data)
+			Users.insert(data)
 			self._clear_entries()
 			self.tree_view.refresh()
 
 	def _update_data(self, widget):
-		pass
-
-	# TODO ask for confirmation
-	def _delete_user(self, widget):
 		user_id = self.tree_view.store[self._selected_iter][0]
-		self.users.delete(user_id)
+		data = self._get_data()
+		if self._is_proper_user_data(data):
+			Users.update(user_id, data)
+			self.tree_view.refresh()
+
+	def _on_delete_clicked(self, widget):
+		self.confirmDialog = ConfirmDeleteDialog(self)
+
+	def _delete_user(self):		
+		user_id = self.tree_view.store[self._selected_iter][0]
+		Users.delete(user_id)	
 		self.tree_view.refresh()
 
-	# TODO clear entries if creating, restore current data in entries if updating
 	def _on_cancel_clicked(self, widget):
 		if self._hasSelected:
 			self._fill_entries_with(self.tree_view.store[self._selected_iter])
@@ -142,7 +145,6 @@ class MainWindow(GladeObject):
 		self.entries["dni"].set_text(data[4])
 		self.entries["address"].set_text(data[5])
 
-
 	def _set_buttons_creation_mode(self):
 		self.buttons["new"].set_sensitive(False)
 		self.buttons["save"].set_sensitive(True)
@@ -162,13 +164,13 @@ class MainWindow(GladeObject):
 
 ###########################################################################################
 
-class TreeView(GladeObject):
+class TreeView():
 	"""Manage users TreeView"""
 
-	def __init__(self):
-		GladeObject.__init__(self)
-		self.this_tree_view = self.glade.get_object("treeView")
-		self.store 			= self.glade.get_object("store")
+	def __init__(self, container_builder):
+		self.builder 		= container_builder
+		self.this_tree_view = self.builder.get_object("treeView")
+		self.store 			= self.builder.get_object("store")
 		self.selection 		= self.this_tree_view.get_selection()
 		
 		self._prepare_columns()
@@ -182,34 +184,65 @@ class TreeView(GladeObject):
 
 	def refresh(self):
 		self.store.clear()
-		users = self.users.list_all()
+		users = Users.list_all()
 		for user in users:
 			self.store.append(user)
+
+###########################################################################################
+
+class ConfirmDeleteDialog(GladeContainer):
+
+	def __init__(self, mainWindow):
+		GladeContainer.__init__(self)
+		
+		self._mainWindow = mainWindow
+		self._this_dialog = self.builder.get_object("ConfirmDeleteDialog")
+		self._confirmButton = self.builder.get_object("confirmDeleteButton")
+		self._cancelButton = self.builder.get_object("cancelDeleteButton")
+
+		self._confirmButton.connect("clicked", self._on_delete_confirmed)
+		self._cancelButton.connect("clicked", lambda widget: self._this_dialog.destroy())
+
+		self._this_dialog.show_all()
+
+	def _on_delete_confirmed(self, widget):
+		self._mainWindow._delete_user()
+		self._this_dialog.destroy()
 
 ###########################################################################################
 
 class Users:
 	"""Model for ´users´ table"""
 
-	def __init__(self):
-		self._conn = sqlite3.connect("main.db")
-		self._conn.text_factory = str
-		self._c = self._conn.cursor()
-		self._table = "users"
+	_conn = sqlite3.connect("main.db")
+	_conn.text_factory = str
+	_c = _conn.cursor()
+	_table = "users"
 
-	def delete(self, userId):
-		delete = "DELETE FROM {0} WHERE id = ?".format(self._table)
-		self._c.execute(delete, (userId,))
-		self._conn.commit()
+	@classmethod
+	def delete(cls, userId):
+		delete = "DELETE FROM {0} WHERE id = ?".format(cls._table)
+		cls._c.execute(delete, (userId,))
+		cls._conn.commit()
 
-	def insert(self, data):
-		insert = "INSERT INTO {0} VALUES (NULL, ?, ?, ?, ?, ?)".format(self._table)
-		self._c.execute(insert, data)
-		self._conn.commit()
+	@classmethod
+	def insert(cls, data):
+		insert = "INSERT INTO {0} VALUES (NULL, ?, ?, ?, ?, ?)".format(cls._table)
+		cls._c.execute(insert, data)
+		cls._conn.commit()
 
-	def list_all(self):
-		self._c.execute("SELECT * FROM {0}".format(self._table))
-		return self._c.fetchall()
+	@classmethod
+	def list_all(cls):
+		cls._c.execute("SELECT * FROM {0}".format(cls._table))
+		return cls._c.fetchall()
+
+	@classmethod
+	def update(cls, user_id, data):
+		update = """UPDATE {0} set surname = ?, mother_surname = ?, name = ?, dni = ?, 
+			address = ? WHERE id = ?""".format(cls._table)
+		data += (user_id,)
+		cls._c.execute(update, data)
+		cls._conn.commit()
 
 ###########################################################################################
 
